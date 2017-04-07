8087ec47036b22b552da26907c5abfac234490d2
Hiroshi Igaki <hirocell@gmail.com>
2017-04-07 16:39:50 +0900

    Kobuki Simple Key operation

---
+++ simple_kobuki_key.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import rospy
+import curses
+import time
+from getch import getch
+from kobuki_msgs.msg import BumperEvent,MotorPower
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+ 
+class SimpleKobuki:
+    def __init__(self):
+        self.bumper_sub = rospy.Subscriber("mobile_base/events/bumper", BumperEvent, self.bumper_cb)
+        self.power_cmd_pub = rospy.Publisher("mobile_base/commands/motor_power", MotorPower,queue_size=10)
+        self.vel_cmd_pub = rospy.Publisher("mobile_base/commands/velocity",Twist,queue_size=10)
+        #self.odm_reset_pub = rospy.Publisher("mobile_base/commands/reset_odometry",Empty)
+        #self.odom_sub = rospy.Subscriber("odom",Odometry, self.odom_cb)
+        self.vel_cmd = Twist()
+
+    def bumper_cb(self, data):
+        if data.state == BumperEvent.PRESSED:
+            print("PRESSED")
+        elif data.state == BumperEvent.RELEASED:
+            print("RELEASED")
+        else:
+            print("Bumper Unknown event")
+
+    def waitConnection(self):
+        while self.power_cmd_pub.get_num_connections() < 1:
+            print("Waiting...")
+            time.sleep(1)
+        print("Connected")
+
+    def spin(self):
+        key = ord(getch())
+        message = ""
+        if key in {83,115}: # S or s
+            message = "START"
+            self.power_cmd_pub.publish(MotorPower(MotorPower.ON))
+        elif key in {81,113}: #Q or q
+            message = "QUIT"
+            self.power_cmd_pub.publish(MotorPower(MotorPower.OFF))
+        elif key == 65: #UP
+            message = "UP"
+            self.vel_cmd.linear.x += 0.05
+        elif key == 66: #DOWN
+            message = "DOWN"
+            self.vel_cmd.linear.x -= 0.05
+        elif key == 67: #RIGHT
+            message = "RIGHT"
+            self.vel_cmd.angular.z -= 0.4
+        elif key == 68: #LEFT 
+            message = "LEFT"
+            self.vel_cmd.angular.z += 0.4
+        elif key == 3:#Ctr+C
+            raise KeyboardInterrupt
+        else:
+            pass
+
+        if message:
+            print(message + " is pressed")
+        self.vel_cmd_pub.publish(self.vel_cmd)
+        print("linear.x = "+str(self.vel_cmd.linear.x)+" angular.z = "+str(self.vel_cmd.angular.z))
+
+def main(args):
+    stdscr = curses.initscr()
+    curses.cbreak()
+    curses.noecho()
+    stdscr.keypad(True)
+    stdscr.timeout(10)
+
+    rospy.init_node("SimpleKobuki_Key", anonymous=True) 
+    kobuki = SimpleKobuki()
+    kobuki.waitConnection()
+    try:
+        while not rospy.is_shutdown():
+            kobuki.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+ 
+if __name__ == '__main__':
+    main(sys.argv)
