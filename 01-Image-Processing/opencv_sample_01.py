cf51c4f374cfba76b6a51ab97f838e3e4f494613
Hiroshi Igaki <hirocell@gmail.com>
2017-04-12 19:07:39 +0900

    clean source code

--- 01-Image-Processing/opencv_sample_01.py
+++ 01-Image-Processing/opencv_sample_01.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-import sys
 import cv2
 import numpy
 
--- 01-Image-Processing/opencv_sample_02.py
+++ 01-Image-Processing/opencv_sample_02.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-import sys
 import cv2
 
 WINNAME = "OpenCV Sample 02"
