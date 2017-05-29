ff273f8a352558202464ce0715b3e550fc059a97
Hiroshi Igaki <github@musictale.net>
2017-05-29 19:28:03 +0900

    Step1

---
+++ 06-MiniGame/minigame_key.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import rospy
+import time
+import curses
+from kobuki_msgs.msg import MotorPower
+from geometry_msgs.msg import Twist
+
+class SimpleKobuki:
+    def __init__(self,scr):
+        self.frame = 0
+        self.scr = scr
+        self.power_cmd_pub = rospy.Publisher("mobile_base/commands/motor_power", MotorPower,queue_size=10)
+        self.vel_cmd_pub = rospy.Publisher("mobile_base/commands/velocity",Twist,queue_size=10)
+        self.vel_cmd = Twist()
+        
+    def waitConnection(self):
+        while self.power_cmd_pub.get_num_connections() < 1:
+            print("Waiting...")
+            time.sleep(1)
+        print("Connected")
+
+    def spin(self):
+        self.frame = self.frame+1
+        key = self.scr.getch()
+        message = ""
+        if key in {83,115}: # S or s
+            message = "START"
+            self.power_cmd_pub.publish(MotorPower(MotorPower.ON))
+        elif key in {81,113}: #Q or q
+            message = "QUIT"
+            self.power_cmd_pub.publish(MotorPower(MotorPower.OFF))
+        elif key == 65: #UP
+            message = "UP"
+            self.vel_cmd.linear.x = 0.1
+            self.vel_cmd.angular.z = 0
+        elif key == 66: #DOWN
+            message = "DOWN"
+            self.vel_cmd.linear.x = -0.1
+            self.vel_cmd.angular.z = 0
+        elif key == 67: #RIGHT
+            message = "RIGHT"
+            self.vel_cmd.linear.x = 0
+            self.vel_cmd.angular.z = -0.4
+        elif key == 68: #LEFT 
+            message = "LEFT"
+            self.vel_cmd.linear.x = 0
+            self.vel_cmd.angular.z = 0.4
+        elif key == 3:#Ctr+C
+            raise KeyboardInterrupt
+
+        if message:
+            message = message + " is pressed "
+        self.scr.move(3,0)
+        self.scr.clrtoeol()
+        self.scr.addstr(3,0,message + "linear.x = "+str(self.vel_cmd.linear.x)+" angular.z = "+str(self.vel_cmd.angular.z)+" frame " + str(self.frame))
+        self.vel_cmd_pub.publish(self.vel_cmd)
+
+def main(args):
+    rospy.init_node("minigame_key", anonymous=True)
+    #init curses setting
+    stdscr = curses.initscr()
+    stdscr.nodelay(1)
+    curses.noecho()
+    
+    #create SimpleKobuki Object
+    kobuki = SimpleKobuki(stdscr)
+    kobuki.waitConnection()
+    
+    #20 means 20Hz.This setting is corresponding to r.sleep()
+    r = rospy.Rate(20)
+    try:
+        while not rospy.is_shutdown():
+            kobuki.spin()
+            stdscr.refresh()
+            r.sleep() #waits 1/20 s
+    except KeyboardInterrupt:
+        print("Shutting down")
+    #Clean up curses.
+    curses.nocbreak()
+    stdscr.keypad(0)
+    curses.echo()
+    curses.endwin()
+ 
+if __name__ == '__main__':
+    os.system("clear")
+    main(sys.argv)
